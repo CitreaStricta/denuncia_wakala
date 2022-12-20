2cfee6a446d7688c56f905467007cff549660fca
Zso <elxnicorojas@gmail.com>
2022-12-20 17:06:20 -0300

    fix

--- lib/pages/detalles.dart
+++ lib/pages/detalles.dart
@@ -222,218 +222,222 @@ class _DetallesState extends State<Detalles> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          // este container si es innecesario (prueba)
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: BackButton(),
-                  ),
-                  // parsear el SECTOR aqui con los datos de la api
-                  Flexible(
-                    fit: FlexFit.tight,
-                    child: Center(
-                      child: Text(
-                        textAlign: TextAlign.center,
-                        post.sector,
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            // este container si es innecesario (prueba)
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: BackButton(),
+                    ),
+                    // parsear el SECTOR aqui con los datos de la api
+                    Flexible(
+                      fit: FlexFit.tight,
+                      child: Center(
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          post.sector,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 54),
-                ],
-              ),
-
-              const SizedBox(height: 10),
-
-              // parsear LA DESCRIPCION aqui con los datos de la api
-              Text(
-                post.descripcion,
-                style: const TextStyle(fontSize: 20),
-              ),
-
-              const SizedBox(height: 10),
-
-              // AQUI LAS DOS IMAGENES QUE SE DEBEN PODER VER Y PODER CLIQUEAR\
-              imagesShower(context),
-
-              const SizedBox(height: 10),
-
-              // parsear AUTOR aqui con los datos de la api
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Publicado por ',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Text(
-                    post.autor,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.blue,
-                    ),
-                  ),
-                  const Text(
-                    '.',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 10),
-
-              // Botones para decir si sigue ahi o si no sigue ahí
-              Row(
-                children: [
-                  // boton para "sigue ahi"
-                  const Spacer(),
-                  const Spacer(),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const StadiumBorder(),
-                      minimumSize:
-                          Size(MediaQuery.of(context).size.width * 0.3, 60),
-                    ),
-                    onPressed: () {
-                      post.sigueAhi = (int.parse(post.sigueAhi) + 1).toString();
-                      setState(() {});
-                      sigueAhi(context);
-                    },
-
-                    /* debe incluir el numero de botos que tiene*/
-                    child: Row(
-                      children: [
-                        const Text("Sigue ahi"),
-                        const SizedBox(width: 10),
-                        Text(
-                          post.sigueAhi,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Spacer(),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const StadiumBorder(),
-                      minimumSize:
-                          Size(MediaQuery.of(context).size.width * 0.3, 60),
-                    ),
-                    onPressed: () {
-                      post.yaNoEsta = (int.parse(post.yaNoEsta) + 1).toString();
-                      setState(() {});
-                      yaNoEsta(context);
-                    },
-
-                    /* debe incluir el numero de botos que tiene*/
-                    child: Row(
-                      children: [
-                        const Text("Ya no está"),
-                        const SizedBox(width: 10),
-                        Text(
-                          post.yaNoEsta,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Spacer(),
-                  const Spacer(),
-                ],
-              ),
-
-              const SizedBox(height: 10),
-              const Text(
-                "Comentarios",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                    const SizedBox(width: 54),
+                  ],
                 ),
-              ),
 
-              checkComentarioPublicado(),
+                const SizedBox(height: 10),
 
-              const SizedBox(height: 10),
-              // aqui poner los comentarios
-
-              Flexible(
-                fit: FlexFit.tight,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16.0),
-                  child: Container(
-                    color: const Color.fromARGB(26, 125, 119, 121),
-                    child: getListView(post.comentarios.length),
-                  ),
+                // parsear LA DESCRIPCION aqui con los datos de la api
+                Text(
+                  post.descripcion,
+                  style: const TextStyle(fontSize: 20),
                 ),
-              ),
 
-              // Text Field de comentario y boton para publicar
-              const SizedBox(height: 10),
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 10,
-                height: 60,
-                child: Row(
+                const SizedBox(height: 10),
+
+                // AQUI LAS DOS IMAGENES QUE SE DEBEN PODER VER Y PODER CLIQUEAR\
+                imagesShower(context),
+
+                const SizedBox(height: 10),
+
+                // parsear AUTOR aqui con los datos de la api
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Flexible(
-                      child: TextField(
-                        controller: commentController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30))),
-                          hintText: "Comentar",
-                        ),
+                    const Text(
+                      'Publicado por ',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      post.autor,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.blue,
                       ),
                     ),
-                    const SizedBox(width: 10),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: const StadiumBorder(),
-                        minimumSize: const Size(100, 60),
-                      ),
-                      onPressed: () {
-                        if (commentController.text.isNotEmpty) {
-                          comentarioPublicado = true;
-                          setState(() {});
-                          postearComentario(
-                            post.id!,
-                            commentController.text,
-                            Global.localId,
-                          );
-                          commentController.text = "";
-                          Timer(const Duration(seconds: 2), () {
-                            setState(() {
-                              comentarioPublicado = false;
-                              setState(() {});
-                            });
-                          });
-                        }
-                      },
-                      child: const Text("Publicar"),
+                    const Text(
+                      '.',
+                      style: TextStyle(fontSize: 16),
                     ),
                   ],
                 ),
-              ),
-            ],
+
+                const SizedBox(height: 10),
+
+                // Botones para decir si sigue ahi o si no sigue ahí
+                Row(
+                  children: [
+                    // boton para "sigue ahi"
+                    const Spacer(),
+                    const Spacer(),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const StadiumBorder(),
+                        minimumSize:
+                            Size(MediaQuery.of(context).size.width * 0.3, 60),
+                      ),
+                      onPressed: () {
+                        post.sigueAhi =
+                            (int.parse(post.sigueAhi) + 1).toString();
+                        setState(() {});
+                        sigueAhi(context);
+                      },
+
+                      /* debe incluir el numero de botos que tiene*/
+                      child: Row(
+                        children: [
+                          const Text("Sigue ahi"),
+                          const SizedBox(width: 10),
+                          Text(
+                            post.sigueAhi,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const StadiumBorder(),
+                        minimumSize:
+                            Size(MediaQuery.of(context).size.width * 0.3, 60),
+                      ),
+                      onPressed: () {
+                        post.yaNoEsta =
+                            (int.parse(post.yaNoEsta) + 1).toString();
+                        setState(() {});
+                        yaNoEsta(context);
+                      },
+
+                      /* debe incluir el numero de botos que tiene*/
+                      child: Row(
+                        children: [
+                          const Text("Ya no está"),
+                          const SizedBox(width: 10),
+                          Text(
+                            post.yaNoEsta,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    const Spacer(),
+                  ],
+                ),
+
+                const SizedBox(height: 10),
+                const Text(
+                  "Comentarios",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                checkComentarioPublicado(),
+
+                const SizedBox(height: 10),
+                // aqui poner los comentarios
+
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16.0),
+                    child: Container(
+                      color: const Color.fromARGB(26, 125, 119, 121),
+                      child: getListView(post.comentarios.length),
+                    ),
+                  ),
+                ),
+
+                // Text Field de comentario y boton para publicar
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 10,
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        child: TextField(
+                          controller: commentController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30))),
+                            hintText: "Comentar",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: const StadiumBorder(),
+                          minimumSize: const Size(100, 60),
+                        ),
+                        onPressed: () {
+                          if (commentController.text.isNotEmpty) {
+                            comentarioPublicado = true;
+                            setState(() {});
+                            postearComentario(
+                              post.id!,
+                              commentController.text,
+                              Global.localId,
+                            );
+                            commentController.text = "";
+                            Timer(const Duration(seconds: 2), () {
+                              setState(() {
+                                comentarioPublicado = false;
+                                setState(() {});
+                              });
+                            });
+                          }
+                        },
+                        child: const Text("Publicar"),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
